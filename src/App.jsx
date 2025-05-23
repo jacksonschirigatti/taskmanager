84620d21c1dc80904f3284b033a26f1b8b3b43f3
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2025-05-22 23:34:31 -0300

    commit v0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
-function App () {
-
-  const [message, ] = useState();
-
-  let message = 1;
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Make Read-me",
+      description: "Create Read-me and edit her",
+      isCompleted: false,
+    },
+  ]);
 
   return (
-    <div>
-      <h1 onClick={message+=1}>{message}</h1>
+    <div className="w-screen h-screen bg-slate-800 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl font-bold text-center text-slate-100">
+          Task Manager
+        </h1>
+        <AddTask></AddTask>
+        <Tasks></Tasks>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
